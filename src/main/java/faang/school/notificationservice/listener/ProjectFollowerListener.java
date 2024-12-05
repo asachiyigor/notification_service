29aa1165ff0c873d51d1e480cee99b2c4a6e8b95
Aleksey <aleksei.zuk86@gmail.com>
2024-12-05 19:55:47 +0300

    notification

--- src/main/java/faang/school/notificationservice/listener/ProjectFollowerListener.java
+++ src/main/java/faang/school/notificationservice/listener/ProjectFollowerListener.java
@@ -2,24 +2,27 @@ package faang.school.notificationservice.listener;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import faang.school.notificationservice.client.UserServiceClient;
+import faang.school.notificationservice.dto.UserDto;
 import faang.school.notificationservice.dto.event.ProjectFollowerEvent;
 import faang.school.notificationservice.messaging.MessageBuilder;
 import faang.school.notificationservice.service.NotificationService;
-import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.redis.connection.Message;
 import org.springframework.data.redis.connection.MessageListener;
 import org.springframework.data.redis.listener.ChannelTopic;
 import org.springframework.stereotype.Component;
-import org.w3c.dom.events.Event;
 
 import java.util.List;
-import java.util.Locale;
 
 @Component
 public class ProjectFollowerListener extends AbstractEventListener<ProjectFollowerEvent> implements MessageListener {
-    public ProjectFollowerListener(List<NotificationService> notificationServices, List<MessageBuilder<ProjectFollowerEvent>> messageBuilders, ObjectMapper objectMapper, UserServiceClient userServiceClient) {
+    private final UserServiceClient userServiceClient;
+
+    public ProjectFollowerListener(List<NotificationService> notificationServices,
+                                   List<MessageBuilder<ProjectFollowerEvent>> messageBuilders,
+                                   ObjectMapper objectMapper, UserServiceClient userServiceClient) {
         super(notificationServices, messageBuilders, objectMapper, userServiceClient);
+        this.userServiceClient = userServiceClient;
     }
 
     @Value("${spring.data.redis.channel.project-follower-channel.name}")
@@ -32,9 +35,10 @@ public class ProjectFollowerListener extends AbstractEventListener<ProjectFollow
 
     @Override
     public void onMessage(Message message, byte[] pattern) {
-        handleEvent(message,ProjectFollowerEvent.class, event ->{
-            String text = getMessage(ProjectFollowerEvent.class,event,Locale.getDefault());
-            sendNotification(event.ownerId(), );
+        handleEvent(message, ProjectFollowerEvent.class, event -> {
+            UserDto user = userServiceClient.getUser(event.ownerId());
+            String text = getMessage(ProjectFollowerEvent.class, event, user.getLocale());
+            sendNotification(event.ownerId(), text);
         });
     }
 }
