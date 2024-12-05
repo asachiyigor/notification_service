a36dd6de665204e182ec7cf61b2290b66445480d
Aleksey <aleksei.zuk86@gmail.com>
2024-12-05 17:18:16 +0300

    необходимый минимум для задачи

--- src/main/java/faang/school/notificationservice/config/redis/RedisSmsProperties.java
+++
@@ -1,7 +0,0 @@
-package faang.school.notificationservice.config.redis;
-
-import org.springframework.boot.context.properties.ConfigurationProperties;
-
-@ConfigurationProperties(prefix = "spring.data.redis")
-public record RedisSmsProperties(Integer port, String host, String userSmsTopic) {
-}
