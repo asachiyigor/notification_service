4d5f723fadd0324eb09f889051b6c16216347c66
AlekseyB86 <avbelov86@mail.ru>
2024-12-07 14:19:33 +0300

    поправил тесты на EmailService

--- src/test/java/faang/school/notificationservice/service/email/EmailServiceTest.java
+++ src/test/java/faang/school/notificationservice/service/email/EmailServiceTest.java
@@ -1,6 +1,7 @@
 package faang.school.notificationservice.service.email;
 
 import faang.school.notificationservice.dto.UserDto;
+import faang.school.notificationservice.validator.mail.EmailValidator;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -14,7 +15,7 @@ import org.springframework.mail.javamail.JavaMailSender;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.*;
 
 
 @ExtendWith(MockitoExtension.class)
@@ -24,6 +25,9 @@ class EmailServiceTest {
 
     @Mock
     private JavaMailSender mailSender;
+
+    @Mock
+    private EmailValidator emailValidator;
 
     @Captor
     private ArgumentCaptor<SimpleMailMessage> messageCaptor;
@@ -45,6 +49,7 @@ class EmailServiceTest {
 
     @Test
     void testSend_Positive() {
+        doNothing().when(emailValidator).checkUserEmailAddress(userDto);
         emailService.send(userDto, "Test");
         verify(mailSender).send(messageCaptor.capture());
         SimpleMailMessage result = messageCaptor.getValue();
@@ -54,19 +59,37 @@ class EmailServiceTest {
     @Test
     void testSend_emailIsNull_Negative() {
         userDto.setEmail(null);
-        assertThrows(IllegalArgumentException.class, () -> emailService.send(userDto, "Test"));
+        doThrow(new IllegalArgumentException("Not found email for user with id=1"))
+                .when(emailValidator)
+                .checkUserEmailAddress(userDto);
+
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class,
+                () -> emailService.send(userDto, "Test"));
+        assertEquals("Not found email for user with id=1", exception.getMessage());
     }
 
     @Test
     void testSend_emailIsBlank_Negative() {
         userDto.setEmail("   ");
-        assertThrows(IllegalArgumentException.class, () -> emailService.send(userDto, "Test"));
+        doThrow(new IllegalArgumentException("Not found email for user with id=1"))
+                .when(emailValidator)
+                .checkUserEmailAddress(userDto);
+
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class,
+                () -> emailService.send(userDto, "Test"));
+        assertEquals("Not found email for user with id=1", exception.getMessage());
     }
 
     @Test
     void testSend_emailNotValid_Negative() {
         userDto.setEmail("user@gmail.c5om");
-        assertThrows(IllegalArgumentException.class, () -> emailService.send(userDto, "Test"));
+        doThrow(new IllegalArgumentException("Not valid email " + userDto.getEmail() + " for user with id=1"))
+                .when(emailValidator)
+                .checkUserEmailAddress(userDto);
+
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class,
+                () -> emailService.send(userDto, "Test"));
+        assertEquals("Not valid email " + userDto.getEmail() + " for user with id=1", exception.getMessage());
     }
 
     @Test
---
+++ src/test/java/faang/school/notificationservice/validator/mail/EmailValidatorTest.java
@@ -0,0 +1,13 @@
+package faang.school.notificationservice.validator.mail;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class EmailValidatorTest {
+
+    @Test
+    void checkUserEmailAddress() {
+
+    }
+}
